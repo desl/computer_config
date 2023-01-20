02e36b89232f7c2a6a11367809e783a3bf4a5a94
Dan Lennon <safarileader@gmail.com>
2023-01-20 15:02:51 -0800

    Create init.lua
    My most basic hammerspoon configuration.

---
+++ init.lua
@@ -0,0 +1,54 @@
+hs.alert.show("Config Reloaded!")
+hs.alert.show("/users/desl/init.lua")
+
+-- Reload Config hotkey
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+
+-- Window MAXIMIZE
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+-- Window to LEFT side of screen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Window to RIGHT side of screen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("/users/desl/init.lua")
+end)
